b87fbf431e710d574d9001aa7053020911de673e
Muzzi Aldean <amiraldean@gmail.com>
2018-04-06 15:14:29 +0100

    Asked for user input to build students list

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-# List students in an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # Get first name
+  name = gets.chomp
+  # While name is not empty, repeat this code
+  while !name.empty? do
+    # Add the student hash to the array
+    students.push << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # Get nother name from user
+    name = gets.chomp
+  end
+  students
+end
 def print_header
   puts "The students of Villains Academy"
   puts "--------------"
@@ -25,6 +28,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 # Call methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
