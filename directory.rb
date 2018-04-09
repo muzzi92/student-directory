b9a84eebd64273e55bfd2bd0d81716c8a54cf123
Muzzi Aldean <amiraldean@gmail.com>
2018-04-09 12:35:30 +0100

    Created interactive main menu

--- directory.rb
+++ directory.rb
@@ -1,39 +1,17 @@
 # Method for inputting student information
 def input_students
   students = []
-  while true do
-    puts "Please enter a student name: "
+  puts "Please enter student names to add them."
+  puts "Press enter twice to exit to the main menu."
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: "April"}
+    puts "Students added so far: #{students.count}"
     name = gets.chomp
-    puts "Please enter cohort month for this student: "
-    cohort = gets.chomp
-    puts "Please enter a hobbie for this student: "
-    hobbie = gets.chomp
-    students << {name: name, cohort: cohort, hobbies: hobbie}
-    puts "Add another student, yes or no?"
-    input = gets.chomp
-    break if input == "no"
   end
   students
 end
-# Method to sort students into hash by cohorts
-def sort_by_cohort(students)
-  sorted_by_cohort = {}
-  students.each do |student|
-    cohort = student[:cohort]
-    if sorted_by_cohort[cohort] == nil
-      sorted_by_cohort[cohort] = []
-    end
-    sorted_by_cohort[cohort].push(student[:name])
-  end
-  sorted_by_cohort
-end
-# Method for printing users by desired cohort
-def print_cohort_hash(hash)
-  puts "What cohort of students would you like to view? "
-  cohort = gets.chomp
-  puts cohort + ":"
-  puts hash[cohort]
-end
+
 # Method to print header text
 def print_header
   puts "The students of Villains Academy".center(50)
@@ -43,19 +21,40 @@ end
 def print(students)
   i = 0
   while i < students.length do
-    if students[i][:name].to_s.chr.downcase == "m" && students[i][:name].length < 12
-      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort) likes #{students[i][:hobbies]}.".center(50)
-    end
+    puts "#{i + 1}. #{students[i][:name]}: (#{students[i][:cohort]} cohort)".center(50)
     i +=1
   end
 end
 # Method to print footer text inc. student count
 def print_footer(students)
     students.count > 1 ? plural = "s" : plural = ""
-    puts "Overall, we have #{students.count} great student#{plural}".center(50)
+    puts "In total, we have #{students.count} great student#{plural}.".center(50)
 end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. Print the menu options and ask user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. Obtain input and save it to a variable
+    input = gets.chomp
+    # 3. Do what the user has asked
+    case input
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      break
+    else
+      puts "I didn't understand the input, try again."
+    end
+  end
+end
+
 # Call methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
